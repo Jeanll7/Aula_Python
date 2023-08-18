8b411aa481eea623133b127fe88143bfbd63e0cb
devjean <jeancarloleall@gmail.com>
2023-08-18 18:59:39 -0300

    color

---
+++ Aula04_dicionários/dicionarios02.py
@@ -0,0 +1,36 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8') 
+
+# Criando um dicionário de cores e seus códigos hexadecimais
+cores = {
+  "vermelho": "#FF0000",
+  "verde": "#00FF00",
+  "azul": "#0000FF"
+}
+
+# Acessando valores usando as chaves
+print("Código de Vermelho:", cores["vermelho"])
+print("Código de Verde:", cores["verde"])
+print("Código de Azul:", cores["azul"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+
+
