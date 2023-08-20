5d7254c0350271147da6370dac898ce418c76215
devjean <jeancarloleall@gmail.com>
2023-08-20 16:27:36 -0300

    dicionario_a4

---
+++ Aula04_dicionários/dicionarios03.py
@@ -0,0 +1,44 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8') 
+
+# Criando um dicionário de nomes e chaves
+nomes_e_chaves = {
+  'Alice': 9955,
+  'Bob': 5678,
+  'Carol': 9012,
+  'David': 3456
+}
+
+# Acessando os valores associados às chaves
+chave_da_Alice = nomes_e_chaves['Alice']
+chave_do_Bob = nomes_e_chaves['Bob']
+
+# Imprimindo os valores associados às chaves
+print(f"A chave da Alice é: {chave_da_Alice}")
+print(f"A chave do Bob é: {chave_do_Bob}")
+
+
+# time: 7:14 - dicionário
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+
+
