fdbcb98171dd67b3102a1b171c954a38128c8965
devjean <jeancarloleall@gmail.com>
2023-09-04 20:09:53 -0300

    randint

---
+++ Aula07_modulo/modulo01.py
@@ -0,0 +1,52 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8') 
+
+# from random import *
+
+# print(randint(1, 30))
+
+from random import randint
+
+erros = 0
+numero = randint(1, 10)
+
+while True:
+  print("Qual o numero correto? ")
+  palpite = int(input())
+  
+  if palpite == numero:
+    print("Parabéns você acertou")
+    break
+  else:
+    # print("Tente novamente")
+    erros += 1  
+
+print(f"Você cometeu {erros} erro(s) durante a partida.")
+  
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+
+
---
+++ Aula07_modulo/rondom.py
@@ -0,0 +1,46 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8') 
+
+# import random
+# import random import choice
+
+"""from random import (  
+  random,
+  choice
+)"""
+
+
+import random as rd
+
+print(rd.random())
+
+numero_aleatorio = rd.random()
+numero_formatado = "{:.6f}".format(numero_aleatorio).replace(".", "").lstrip("0")
+print(numero_formatado)
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+
+
